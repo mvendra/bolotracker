7697ad6c42ceac5cdee2b097b189476c2f1e97b9
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-09-12 18:41:30 -0300

    signed int to str and vice versa, utils conversion

--- impl/src/utils/conversions.cpp
+++ impl/src/utils/conversions.cpp
@@ -10,6 +10,13 @@ unsigned int strToUint(const std::string &src) {
     return ret;
 }
 
+int strToInt(const std::string &src){
+    int ret;
+    std::stringstream ss(src);
+    ss >> ret;
+    return ret;
+}
+
 unsigned short strToUshort(const std::string &src){
     unsigned short ret;
     std::stringstream ss(src);
@@ -18,6 +25,14 @@ unsigned short strToUshort(const std::string &src){
 }
 
 std::string uintToStr(const unsigned int num){
+    std::string ret {""};
+    std::stringstream ss;
+    ss << num;
+    ret = ss.str();
+    return ret;
+}
+
+std::string intToStr(const int num){
     std::string ret {""};
     std::stringstream ss;
     ss << num;
--- impl/src/utils/conversions.h
+++ impl/src/utils/conversions.h
@@ -6,8 +6,10 @@
 
 
 unsigned int strToUint(const std::string &src);
+int strToInt(const std::string &src);
 unsigned short strToUshort(const std::string &src);
 std::string uintToStr(const unsigned int num);
+std::string intToStr(const int num);
 std::string ushortToStr(const unsigned int num);
 
 #endif // __CONVERSIONS_H__
