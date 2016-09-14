968be77ce97c12f27c0c3ea91da37561e19c44b5
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-09-14 18:31:28 -0300

    hardening the constraints

--- basic_template.sql
+++ basic_template.sql
@@ -1,33 +1,36 @@
 
 CREATE TABLE investors (
-pk_investor INTEGER PRIMARY KEY,
+pk_investor INTEGER PRIMARY KEY NOT NULL,
 
-name TEXT,
+name TEXT NOT NULL,
 email TEXT,
 description TEXT,
-date_of_inclusion TEXT /* sqlite 3 does not have a date dataype (sept/16) */
+date_of_inclusion TEXT, /* sqlite 3 does not have a date dataype (sept/16) */
+UNIQUE(pk_investor, name)
 );
 
 CREATE TABLE subjects (
-pk_subject INTEGER PRIMARY KEY,
+pk_subject INTEGER PRIMARY KEY NOT NULL,
 
-tag TEXT, /* (project, subproject, task type, whatever) */
+tag TEXT NOT NULL, /* (project, subproject, task type, whatever) */
 description TEXT,
-date_of_inclusion TEXT
+date_of_inclusion TEXT,
+UNIQUE(pk_subject, tag)
 );
 
 CREATE TABLE currencies (
-pk_currency INTEGER PRIMARY KEY,
+pk_currency INTEGER PRIMARY KEY NOT NULL,
 
-label TEXT,
+label TEXT NOT NULL,
 description TEXT,
-date_of_inclusion TEXT
+date_of_inclusion TEXT,
+UNIQUE(pk_currency, label)
 );
 
 CREATE TABLE invested_time (
-pk_invested_time INTEGER PRIMARY KEY,
-fk_investor INTEGER,
-fk_currency INTEGER,
+pk_invested_time INTEGER PRIMARY KEY NOT NULL,
+fk_investor INTEGER NOT NULL,
+fk_currency INTEGER NOT NULL,
 
 date TEXT,
 description TEXT,
@@ -36,12 +39,13 @@ minutes INTEGER,
 price_per_unit REAL,
 
 FOREIGN KEY(fk_investor) REFERENCES investors(pk_investor),
-FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency)
+FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency),
+UNIQUE(pk_invested_time)
 );
 
 CREATE TABLE invested_time_subjects_link (
-fk_invested_time INTEGER,
-fk_subject INTEGER,
+fk_invested_time INTEGER NOT NULL,
+fk_subject INTEGER NOT NULL,
 
 PRIMARY KEY (fk_invested_time, fk_subject),
 FOREIGN KEY(fk_invested_time) REFERENCES invested_time(pk_invested_time),
@@ -49,9 +53,9 @@ FOREIGN KEY(fk_subject) REFERENCES subjects(pk_subject)
 );
 
 CREATE TABLE invested_assets (
-pk_invested_asset INTEGER PRIMARY KEY,
-fk_investor INTEGER,
-fk_currency INTEGER,
+pk_invested_asset INTEGER PRIMARY KEY NOT NULL,
+fk_investor INTEGER NOT NULL,
+fk_currency INTEGER NOT NULL,
 
 date TEXT,
 short_name TEXT,
@@ -60,12 +64,13 @@ comment TEXT,
 price REAL,
 
 FOREIGN KEY(fk_investor) REFERENCES investors(pk_investor),
-FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency)
+FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency),
+UNIQUE(pk_invested_asset)
 );
 
 CREATE TABLE invested_assets_subjects_link (
-fk_invested_asset INTEGER,
-fk_subject INTEGER,
+fk_invested_asset INTEGER NOT NULL,
+fk_subject INTEGER NOT NULL,
 
 PRIMARY KEY (fk_invested_asset, fk_subject),
 FOREIGN KEY(fk_invested_asset) REFERENCES invested_assets(pk_invested_asset),
@@ -73,8 +78,8 @@ FOREIGN KEY(fk_subject) REFERENCES subjects(pk_subject)
 );
 
 CREATE TABLE bonuses (
-pk_bonus INTEGER PRIMARY KEY,
-fk_investor INTEGER,
+pk_bonus INTEGER PRIMARY KEY NOT NULL,
+fk_investor INTEGER NOT NULL,
 
 date TEXT,
 short_name TEXT,
@@ -82,12 +87,13 @@ description TEXT,
 comment TEXT,
 reward TEXT,
 
-FOREIGN KEY(fk_investor) REFERENCES investors(pk_investor)
+FOREIGN KEY(fk_investor) REFERENCES investors(pk_investor),
+UNIQUE(pk_bonus)
 );
 
 CREATE TABLE bonuses_subjects_link (
-fk_bonus INTEGER,
-fk_subject INTEGER,
+fk_bonus INTEGER NOT NULL,
+fk_subject INTEGER NOT NULL,
 
 PRIMARY KEY (fk_bonus, fk_subject),
 FOREIGN KEY(fk_bonus) REFERENCES bonuses(pk_bonus),
@@ -95,9 +101,9 @@ FOREIGN KEY(fk_subject) REFERENCES subjects(pk_subject)
 );
 
 CREATE TABLE invested_money (
-pk_invested_money INTEGER PRIMARY KEY,
-fk_investor INTEGER,
-fk_currency INTEGER,
+pk_invested_money INTEGER PRIMARY KEY NOT NULL,
+fk_investor INTEGER NOT NULL,
+fk_currency INTEGER NOT NULL,
 
 date TEXT,
 short_name TEXT,
@@ -106,12 +112,13 @@ comment TEXT,
 amount REAL,
 
 FOREIGN KEY(fk_investor) REFERENCES investors(pk_investor),
-FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency)
+FOREIGN KEY(fk_currency) REFERENCES currencies(pk_currency),
+UNIQUE(pk_invested_money)
 );
 
 CREATE TABLE invested_money_subjects_link (
-fk_invested_money INTEGER,
-fk_subject INTEGER,
+fk_invested_money INTEGER NOT NULL,
+fk_subject INTEGER NOT NULL,
 
 PRIMARY KEY (fk_invested_money, fk_subject),
 FOREIGN KEY(fk_invested_money) REFERENCES invested_money(pk_invested_money),
