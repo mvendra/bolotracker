c2b6e8dfbcf57d53b94e2132f9f4cb8a55231357
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-09-17 16:09:27 -0300

    added helper header file to include all dataobjects at once

---
+++ impl/src/dataobjects/dataobjects.h
@@ -0,0 +1,17 @@
+
+#ifndef __DATAOBJECTS_H__
+#define __DATAOBJECTS_H__
+
+#include "bonuses_subjects_link.h"
+#include "currency.h"
+#include "invested_asset.h"
+#include "invested_money.h"
+#include "invested_time_subjects_link.h"
+#include "subject.h"
+#include "bonus.h"
+#include "dataobjects.h"
+#include "invested_assets_subjects_link.h"
+#include "invested_time.h"
+#include "investor.h"
+
+#endif // __DATAOBJECTS_H__
