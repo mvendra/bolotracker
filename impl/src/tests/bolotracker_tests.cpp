a1361e79b3e5a3dfa238317159fb254d887ca120
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-09-19 08:57:19 -0300

    activating all tests

--- impl/src/tests/bolotracker_tests.cpp
+++ impl/src/tests/bolotracker_tests.cpp
@@ -14,8 +14,8 @@ bool test_all(){
 
     bool result {true};
 
-    //test_accrue(result, test_datehelper());
-    //test_accrue(result, test_database());
+    test_accrue(result, test_datehelper());
+    test_accrue(result, test_database());
     test_accrue(result, test_model());
 
     return result;
