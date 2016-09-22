ab630506ab513393bc8f0b488a765380e3ca8afc
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-09-22 09:07:25 -0300

    added unique ptrs to the gui fltk sample. still leaks.

--- impl/src/gui_fltk.cpp
+++ impl/src/gui_fltk.cpp
@@ -4,6 +4,7 @@
 #include "fltk_wrapper.h"
 
 #include <stdlib.h>
+#include <memory>
 
 Gui_FLTK::Gui_FLTK(int _argc, char *_argv[], Model &md):ControllerInterface{_argc, _argv, md}{
 }
@@ -13,8 +14,8 @@ Gui_FLTK::~Gui_FLTK(){
 
 bool Gui_FLTK::run() {
 
-    Fl_Double_Window *window = new Fl_Double_Window(400,320,argv[0]);
-    window->resizable(*(new Fl_Box(FL_ENGRAVED_FRAME,10,10,300,300,
+    std::unique_ptr<Fl_Double_Window> window = std::make_unique<Fl_Double_Window>(400,320,argv[0]);
+    std::unique_ptr<Fl_Box> box = std::make_unique<Fl_Box>(FL_ENGRAVED_FRAME,10,10,300,300,
         "MINIMUM UPDATE TEST\n"
         "\n"
         "The slider on the right purposely\n"
@@ -25,14 +26,16 @@ bool Gui_FLTK::run() {
         "other than hiding and showing\n"
         "of that portion of the window\n"
         "or changing the button that\n"
-        "intesects them.")));
+        "intesects them.");
 
-    Fl_Slider *s;
-    s = new Fl_Slider(320,10,20,300,"Too_Big_Label");
-    s->align(0);
 
-    new Fl_Button(20,270,100,30,"Button");
-    new Fl_Return_Button(200,270,100,30,"Button");
+    window->resizable(box.get());
+
+    std::unique_ptr<Fl_Slider> slider = std::make_unique<Fl_Slider>(320,10,20,300,"Too_Big_Label");
+    slider->align(0);
+
+    std::unique_ptr<Fl_Button> btn1 = std::make_unique<Fl_Button>(20,270,100,30,"Button");
+    std::unique_ptr<Fl_Return_Button> btn2 = std::make_unique<Fl_Return_Button>(200,270,100,30,"Button");
 
     window->show(argc, argv);
     int finish = Fl::run();
